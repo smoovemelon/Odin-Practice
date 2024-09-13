b24717837910cebfc03e7c3c8d547bfba16f5d3d
smoovemelon <Adrinn.nguyensene@gmail.com>
2024-09-12 19:25:12 -0500

    Practiced with JS

--- js-practice/js-assignments/my-script.js
+++ js-practice/js-assignments/my-script.js
@@ -1,3 +1,5 @@
+/* if...else statements 
+
 let shoppingDone = false;
 let childsAllowance;
 
@@ -6,3 +8,26 @@ if (shoppingDone === true) {
 } else {
   childsAllowance = 5;
 }
+
+*/
+
+const select = document.querySelector("select");
+const para = document.querySelector("p");
+
+select.addEventListener("change", setEmotion);
+
+function setEmotion() {
+  const choice = select.value;
+}
+
+if (choice === "happy"); {
+  para.textContent = "Comedy";
+} else if (choice === "sad"); {
+  para.textContent = "Drama";
+} else if (choice === "fear"); {
+  para.textContent = "Horror"; 
+} else if (choice === "anger"); {
+  para.textContent = "Action";
+} else {
+  para.textContent = "";
+  }
