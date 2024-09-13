064485a5ed8718616a52c25456539f01863d7e6f
smoovemelon <adrinn.nguyensene@gmail.com>
2024-09-12 22:15:30 -0500

    Finished mood selector

--- js-practice/js-assignments/my-script.js
+++ js-practice/js-assignments/my-script.js
@@ -1,20 +1,20 @@
-const select = document.querySelector("select");
+const select = document.querySelector("#emotion");
 const para = document.querySelector("p");
 
 select.addEventListener("change", setEmotion);
 
 function setEmotion() {
   const choice = select.value;
-}
 
-if (choice === "happy") {
+    if (choice === "happy") {
     para.textContent = "Comedy";
-} else if (choice === "sad") {
-    para.textContent = "Drama";
-} else if (choice === "angry") {
-    para.textContent = "Action"; 
-} else if (choice === "fear") {
-    para.textContent = "Horror"; 
-} else {
-    para.textContent = "";
+    } else if (choice === "sad") {
+        para.textContent = "Drama";
+    } else if (choice === "angry") {
+        para.textContent = "Action"; 
+    } else if (choice === "fear") {
+        para.textContent = "Horror"; 
+    } else {
+        para.textContent = "";
+    }
 }
