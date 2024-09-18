2fc758214a4f5546e61896893c5e1f6e12686b2e
smoovemelon <Adrinn.nguyensene@gmail.com>
2024-09-18 15:51:05 -0500

    JS practice

--- js-practice/js-assignments/my-script.js
+++ js-practice/js-assignments/my-script.js
@@ -1,3 +1,4 @@
+/* 
 const select = document.querySelector("select");
 const para = document.querySelector("p");
 
@@ -14,7 +15,53 @@ function setPoop() {
     para.textContent = "";
   }
 }
+ */
 
+/* 
+let age = prompt("What is your age?");
 
+if (age > 18) {
+  alert("Wow you're old!");
+  alert("Get a cane!");
+} else if (age == 18) {
+  alert("Sheesh, you're barely there!")
+} else {
+  alert("You're still a child!")
+  alert("Get out of here!")
+}
+ */
+
+/* 
+let age = prompt('age?');
+
+let message = (age < 3) ? "You're a baby!":
+(age < 18) ? "Hello!":
+(age < 100) ? "Greetings!":
+"Wow! You're old!"
+
+alert(message);
+ */
+
+/* 
+let lbs = prompt
+("How much do you weigh? Just include the number!");
+
+let message = (lbs < 85) ? "You're really light!":
+  lbs < 120 ? "You're pretty average.":
+  lbs < 180 ? "You're on the bulkier side.":
+  lbs < 240 ? "You're a fatty!":
+  "I think you need to lose A LOT of weight...";
+
+alert(message);
+ */
+
+let fatty = prompt("Is Boobear a fatty? (yes/no)");
+
+if (fatty == "yes") {
+  alert("Why yes you are!");
+} else {
+  alert("Yes you are!!!");
+}
+  
   
 
