d6906d93ba26ada8e366a9bf7e742927e71aff89
smoovemelon <Adrinn.nguyensene@gmail.com>
2024-09-18 22:14:07 -0500

    n/a

--- js-practice/js-assignments/my-script.js
+++ js-practice/js-assignments/my-script.js
@@ -127,31 +127,4 @@ switch (a) {
     break;
 }
  */
-=======
 
-    if (choice === "happy") {
-    para.textContent = "Comedy";
-    } else if (choice === "sad") {
-        para.textContent = "Drama";
-    } else if (choice === "angry") {
-        para.textContent = "Action"; 
-    } else if (choice === "fear") {
-        para.textContent = "Horror"; 
-    } else {
-        para.textContent = "";
-    }
-}
-/* 
-    let days = 31;
-    if (choice === February) {
-        days = 28;
-    } else if (
-        choice === "April" ||
-        choice === "June" ||
-        choice ==="September" ||
-        choice === "November"
-    ) {
-        days = 30;
-    }
-*/
->>>>>>> 8de45cea0dd83a6ca9cb8b2ec114448d1bb2d318
